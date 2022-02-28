bebcd6229ab4447cc25e29356ae406f663e84ab3
João Rodrigo Prieto <jrp2033@gmail.com>
2022-02-28 11:46:39 -0300

    Project base

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Home.module.scss'
+
+export default function signIn() {
+  return (
+    <div className={styles.container}>
+      <h1>hello world</h1>
+    </div>
+  )
+}
