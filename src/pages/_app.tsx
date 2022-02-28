175faed64d6afd3908e7a8c76d32cf7a2792cca0
João Rodrigo Prieto <jrp2033@gmail.com>
2022-02-28 11:54:34 -0300

    others changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import '../styles/globals.scss'
+import { AuthProvider } from '../contexts/AuthContext'
+import { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
 }
 
 export default MyApp
