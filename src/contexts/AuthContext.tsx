89b4c9a6348dc44d8e0f4c3b4ffd315f57ea4d0f
João Rodrigo Prieto <jrp2033@gmail.com>
2022-03-01 12:58:48 -0300

    login screen

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -3,42 +3,47 @@ import { setCookie, parseCookies, destroyCookie } from 'nookies';
 
 import { api } from "../services/api";
 
+interface User {
+    id: string;
+    name: string;
+    birthday: string;
+    gerder: string;
+}
+
+
 export const AuthContext = createContext({});
 
-export function AuthProvider({children}){
-    // const [user, setUser] = useState();
+export function AuthProvider({ children }) {
+    const [user, setUser] = useState<User>();
     // const isAuthenticated = !!user;
 
-    async function signIn({email, password}){
+    async function signIn({ email, password }) {
         try {
-            const response = await api.post('users/auth/sign_in', {
+            const response = await api.post('/auth/sign-in', {
                 email,
                 password
             });
 
-            setCookie(undefined, 'ioasys.access-token', response.headers["access-token"], {
+            setCookie(undefined, 'ioasys.token', response.headers["Authorization"], {
                 maxAge: 60 * 60 * 24 * 30, // 30 days
                 path: '/'
             });
-            setCookie(undefined, 'ioasys.client', response.headers["client"], {
-                maxAge: 60 * 60 * 24 * 30, // 30 days
-                path: '/'
-            });
-            setCookie(undefined, 'ioasys.uid', response.headers["uid"], {
+            setCookie(undefined, 'ioasys.refreshToken', response.headers["refresh-token"], {
                 maxAge: 60 * 60 * 24 * 30, // 30 days
                 path: '/'
             });
 
-            // console.log(response);
+
+            console.log(response);
 
             return true;
 
         } catch (error) {
-            console.error(error);   
-            return false;         
+            console.error(error);
+            return false;
         }
     }
-    
+
     return (
         <AuthContext.Provider value={{ signIn }}>
             {children}
--- src/services/apiAuth.ts
+++ src/services/apiAuth.ts
@@ -6,13 +6,12 @@ export function apiAuth() {
     const cookies = parseCookies(undefined);
 
     const api = axios.create({
-        baseURL: 'https://empresas.ioasys.com.br/api/v1/',
-        headers: {
-            'access-token': `${cookies['ioasys.access-token']}`,
-            'client': `${cookies['ioasys.client']}`,
-            'uid': `${cookies['ioasys.uid']}`,
-
-        }
+        baseURL: 'https://books.ioasys.com.br/api/v1',
+        // headers: {
+        //     'access-token': `${cookies['ioasys.access-token']}`,
+        //     'client': `${cookies['ioasys.client']}`,
+        //     'uid': `${cookies['ioasys.uid']}`,
+        // }
     })
 
     return api;
