0d0b534b48faafe7f642c923a87fb8993b6c68a5
João Rodrigo Prieto <jrp2033@gmail.com>
2022-03-09 13:27:57 -0300

    buttons hover

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,10 @@
-import Image from 'next/image';
+
 import { useContext } from 'react';
+import Image from 'next/image';
 import { AuthContext } from '../../contexts/AuthContext';
+
+import { BsArrowBarRight } from "react-icons/bs";
+
 import styles from './header.module.scss';
 
 export default function Header() {
@@ -14,7 +18,9 @@ export default function Header() {
             <span><Image className={styles.logo} src="/images/logo2.png" alt="logo" width={90} height={35} /><p>Books</p></span>
             <div>
                 <p>Bem vindo, <strong>{user.name}</strong></p>
-                <button onClick={signOut}><Image src="/images/shape.png" alt="signOut" width={6} height={17} /><Image src="/images/shape2.png" alt="signOut2" width={13} height={12} /></button>
+                {/* <button onClick={signOut}><Image src="/images/shape.svg" alt="signOut" width={6} height={17} /><Image src="/images/shape2.svg" alt="signOut2" width={13} height={12} /></button> */}
+                <button onClick={signOut}><BsArrowBarRight /></button>
+
             </div>
         </div>
     )
